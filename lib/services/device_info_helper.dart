400a3ba5d2d9130551c19e5d642814cc9737f6ba
Leeous <contact@leeous.com>
2026-08-12 16:13:52 -0400

    feat: add DeviceInfoHelper for retrieving host device names

---
+++ lib/services/device_info_helper.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+import 'package:device_info_plus/device_info_plus.dart';
+
+class DeviceInfoHelper {
+  static Future<String> getHostDeviceName() async {
+    final deviceInfo = DeviceInfoPlugin();
+
+    try {
+      if (Platform.isAndroid) {
+        final androidInfo = await deviceInfo.androidInfo;
+        return androidInfo.name;
+      }
+
+      if (Platform.isLinux) {
+        final linuxInfo = await deviceInfo.linuxInfo;
+        return linuxInfo.prettyName;
+      }
+
+      if (Platform.isWindows) {
+        final windowsInfo = await deviceInfo.windowsInfo;
+        return windowsInfo.computerName;
+      }
+    } catch (_) {}
+
+    return 'Vizio Remote';
+  }
+}
