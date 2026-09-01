3c56654d3d39cbdf75dcad27fc7cc7df8f02f800
Leeous <contact@leeous.com>
2026-09-01 14:24:18 -0400

    refactor: enhance dPrint function to accept multiple optional parameters and print non-null values

--- lib/services/logging_helper.dart
+++ lib/services/logging_helper.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/foundation.dart';
 
-void dPrint(Object? object) {
-  if (kDebugMode) print(object);
+void dPrint(Object? o1, [Object? o2, Object? o3, Object? o4]) {
+  if (kDebugMode) {
+    final items = [o1, o2, o3, o4].where((e) => e != null);
+    print(items);
+  }
 }
